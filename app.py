664c8d9c04d16bd023638ffe766432b49408de87
antonio.campello <antonio.campello@icloud.com>
2018-09-02 18:54:15 +0100

    Added rejected visas

--- app.py
+++ app.py
@@ -10,17 +10,13 @@ app = dash.Dash()
 server = app.server
 
 dictionaries_of_years = [{'label': year, 'value': year} for year in np.arange(2005, 2018)]
-dictionaries_of_visas = [{'label': 'Work', 'value': 'work'}, {'label': 'Study', 'value': 'study'}]
+
 dictionaries_of_outcomes = [{'label': 'Granted', 'value': 'granted'},
                             {'label': 'Rejected', 'value': 'rejected'}]
 
 dropdown_years = dcc.Dropdown(id='year_to_display',
                               options=dictionaries_of_years,
                               value='2016')
-
-dropdown_types_of_visa = dcc.Dropdown(id='type_of_visa_to_display',
-                                      options=dictionaries_of_visas,
-                                      value='work')
 
 radio_items_type_of_outcome = dcc.RadioItems(id='outcome_of_application',
                                              options=dictionaries_of_outcomes,
@@ -29,10 +25,6 @@ radio_items_type_of_outcome = dcc.RadioItems(id='outcome_of_application',
                                                          'margin-right': 10,
                                                          'margin-top': 30})
 
-checkbox_exclude_commonwealth = dcc.Checklist(id='exclude_commonwealth',
-                                              options=[{'label': 'Exclude Commonwealth', 'value': 'commonwealth'}],
-                                              values=[],
-                                              labelStyle={'margin-top': 30})
 
 # Loads/cache data in a hidden div
 app.layout = html.Div([html.Div(id="data-loading", style={'display': 'none'}),
@@ -49,9 +41,14 @@ app.layout = html.Div([html.Div(id="data-loading", style={'display': 'none'}),
 
                        html.Div([html.Div([html.Label('Year'),
                                            dropdown_years,
-                                           html.Label('Type of Visa',style={"margin-top": "30px"}),
-                                           dropdown_types_of_visa, html.P(''), radio_items_type_of_outcome, html.P(''),
-                                           checkbox_exclude_commonwealth],
+                                           html.Label('Type of Visa(*)',style={"margin-top": "30px"}),
+                                           html.Div([dcc.Dropdown(id='type_of_visa_to_display',
+                                                                  options=[{'label': 'Work', 'value': 'work'},
+                                                                           {'label': 'Study', 'value': 'study'},
+                                                                           {'label': 'All (incl. visitors)', 'value': 'all'}],
+                                                                  value='work')],
+                                                    id='type_of_visa_to_display_div'),
+                                           html.P(''), radio_items_type_of_outcome],
                                           className="three columns", style={"margin-top": 40}),
                                  html.Div([dcc.Graph(id='map')], className="three columns")],
                                 className="row"),
@@ -60,16 +57,59 @@ app.layout = html.Div([html.Div(id="data-loading", style={'display': 'none'}),
                                                      style={"margin-left": 0})], className="five columns"),
                                  html.Div([dcc.Graph(id='graph-total', config={"displayModeBar": False})],
                                           className="seven columns")],
-                                className="row")],
+                                className="row"),
+                       html.Hr(),
+                       html.Div(
+                       dcc.Markdown('''
+(*) Work visas include mainly High-value (Tier 1), Skilled (Tier 2), Youth mobility and
+temporary workers (Tier 5), as well as non-PBS/Other work visas.
+Student visas include mainly Tier 4 (excluding short-term study). 
+Sources: [gov.uk work-visas](https://www.gov.uk/browse/visas-immigration/work-visas) and
+[gov.uk student-visas](https://www.gov.uk/browse/visas-immigration/student-visas).
+
+(**) Rejected applications are only available per country including all categories, and visitors. A FOIA request has 
+been made for the missing information.                    
+                       ''')
+                       )],
                       className='container')
+
+
+@app.callback(dash.dependencies.Output('type_of_visa_to_display', 'options'),
+              [dash.dependencies.Input('outcome_of_application', 'value')])
+def disable_types(selected_outcome):
+    """
+    Callback to disable "work of study" in case of "rejected outcomes
+    :param selected_outcome:
+    :return:
+    """
+    if selected_outcome == "rejected":
+        dictionaries_of_visas = [{'label': 'Work', 'value': 'work', 'disabled': True}, {'label': 'Study', 'value': 'study',
+                                                                                        'disabled': True},
+                                 {'label': 'All (incl. visitors)', 'value': 'all'}]
+    else:
+        dictionaries_of_visas = [{'label': 'Work', 'value': 'work'}, {'label': 'Study', 'value': 'study'},
+                                 {'label': 'All (incl. visitors)', 'value': 'all'}]
+
+    return dictionaries_of_visas
+
+
+@app.callback(dash.dependencies.Output('type_of_visa_to_display', 'value'),
+              [dash.dependencies.Input('outcome_of_application', 'value')])
+def disable_types(selected_outcome):
+    """
+    Callback to pass to "all" in case of "rejected outcomes"
+    :param selected_outcome:
+    :return:
+    """
+
+    return "all"
 
 
 @app.callback(dash.dependencies.Output('data-loading', 'children'),
               [dash.dependencies.Input('year_to_display', 'value'),
                dash.dependencies.Input('type_of_visa_to_display', 'value'),
-               dash.dependencies.Input('outcome_of_application', 'value'),
-               dash.dependencies.Input('exclude_commonwealth', 'values')])
-def load_data(selected_year, selected_type, selected_outcome, selected_exclusion):
+               dash.dependencies.Input('outcome_of_application', 'value')])
+def load_data(selected_year, selected_type, selected_outcome):
     """
 
     Callback to load data from dataframe and serve it to a hidden div, jsonified.
@@ -77,7 +117,6 @@ def load_data(selected_year, selected_type, selected_outcome, selected_exclusion
     :param selected_year: int: Year of visa
     :param selected_type: str: type of visa (Tier 2 or Tier 4)
     :param selected_outcome: str: granted or rejected
-    :param selected_exclusion: bool: Whether to include commonwealth countries or not
 
     :return: list [df, filters]
              df is a jsonified dataframe after applying all the filters and
@@ -86,11 +125,14 @@ def load_data(selected_year, selected_type, selected_outcome, selected_exclusion
 
     filters = {"selected_year": selected_year,
                "selected_type": selected_type,
-               "selected_outcome": selected_outcome,
-               "selected_exclusion": selected_exclusion}
+               "selected_outcome": selected_outcome}
 
     # Reads the correct dataframe
-    file_to_read = 'map_of_immigration_' + str(selected_type) + '_' + str(selected_year) + '.csv'
+    if selected_outcome == 'rejected':
+        file_to_read = 'map_of_immigration_rejected_' + str(selected_year) + '.csv'
+    else:
+        file_to_read = 'map_of_immigration_' + str(selected_type) + '_' + str(selected_year) + '.csv'
+
     data_frame = pd.read_csv('data/' + file_to_read, index_col=0)
 
     # Dumps a json to be able to share across divs
