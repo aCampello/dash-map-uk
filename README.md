081b8428126284f7a7f8e73de0a4030d84ddceff
antonio.campello <antonio.campello@icloud.com>
2018-09-02 19:18:13 +0100

    Change Title

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
-# World map of work immigration to the United Kingdom (based on freely available data https://www.gov.uk/government/publications/immigration-statistics-october-to-december-2016/work#data-tables)
+## DASH APP World map of work immigration to the United Kingdom
 
-Steps:
+The following repository contains a dash app to visualise open data with immigration statistics in the UK.
+It is based on data [made pubicly available by the Home Office](https://www.gov.uk/government/publications/immigration-statistics-october-to-december-2016/work#data-tables).
 
-- Loads data
-- Draws a world map
-- Puts data into 
+It is based on the [Dash]() framework for webapps and and [plot.ly]() for responsive graphs.
 
-# Introduction 
+
+A live version can be found on [https://dash-map-uk.herokuapp.com](https://dash-map-uk.herokuapp.com).
+
+To run locally:
+
+```
+git clone <this_repository>
+virtualenv -p python3 env/
+pip install -r requirements.txt
+python app.py
+``` 
+ 
+ 
+
+### Foreword and key facts 
 
 Every year, over 180,000 non-EU nationals (like myself) apply for a work or student visa to come to the United Kingdom. These indivduals are highly skilled workers, enterpreneurs, sportpersons, etc. Typically the application process involves proving to the Home Office that the visa is intended to fill a technical gap in the country, which might involve showing that the job is in the shortage occupations list or passing through a residents labours market test. A complete list of companies that sponsor or have sponsored work visas can be found here. 
  
-The present dashboard shows the evolution of legal work immigration since 2005. It is based on freely available official This interactive visualisation accounts for statistics of legal immigration in the UK. The points-based system visa has been introduced in 2005 and fully implemented in 2008 and coincides with a drop of more than 37\% in the total work visas. Since 2009, the UK started a policy called [hostile enviroment](https://en.wikipedia.org/wiki/Home_Office_hostile_environment_policy).  
-
-# Some facts
+The present dashboard shows the evolution of legal work immigration since 2005. It is based on freely available official This interactive visualisation accounts for statistics of legal immigration in the UK. The points-based system visa was proposed in 2005 and started to be fully implemented in 2008, coincides with a drop of more than 37\% in the total work visas. Further, in 2009,  the UK started a policy called [hostile enviroment](https://en.wikipedia.org/wiki/Home_Office_hostile_environment_policy), which claims to have reduced illegal immigration, but data is not available.  
 
 Work-related visas are: 
 
 High-value (Tier 1), Skilled (Tier 2), Youth mobility and temporary workers (Tier 5), as well as non-PBS/Other work visas
+
+Student-related visas mainly include Tier 4 (and excludes short-term study). Statistics for rejected applications are only available per country including all categories, and visitors. A FOIA request has been made for the missing information.
 
 # Todo list
 
--- app.py
+++ app.py
@@ -7,26 +7,21 @@ import numpy as np
 import json
 
 app = dash.Dash()
+app.title = 'Map of Immigration'
 server = app.server
 
+# Main dropdown buttons
 dictionaries_of_years = [{'label': year, 'value': year} for year in np.arange(2005, 2018)]
+dictionaries_of_outcomes = [{'label': 'Granted', 'value': 'granted'}, {'label': 'Rejected', 'value': 'rejected'}]
 
-dictionaries_of_outcomes = [{'label': 'Granted', 'value': 'granted'},
-                            {'label': 'Rejected', 'value': 'rejected'}]
+dropdown_years = dcc.Dropdown(id='year_to_display', options=dictionaries_of_years, value='2016')
+radio_items_type_of_outcome = dcc.RadioItems(id='outcome_of_application', options=dictionaries_of_outcomes,
+                                             value='granted', labelStyle={'display': 'inline-block',
+                                                                          'margin-right': 10,
+                                                                          'margin-top': 30})
 
-dropdown_years = dcc.Dropdown(id='year_to_display',
-                              options=dictionaries_of_years,
-                              value='2016')
-
-radio_items_type_of_outcome = dcc.RadioItems(id='outcome_of_application',
-                                             options=dictionaries_of_outcomes,
-                                             value='granted',
-                                             labelStyle={'display': 'inline-block',
-                                                         'margin-right': 10,
-                                                         'margin-top': 30})
-
-
-# Loads/cache data in a hidden div
+# The html structure of the file
+# Loads/cache data in a hidden div for more reative app
 app.layout = html.Div([html.Div(id="data-loading", style={'display': 'none'}),
                        html.Div([html.H4('Map of Legal Immigration in the UK', style={"margin-bottom": 0,
                                                                                       "margin-top": "30px",
@@ -38,7 +33,6 @@ app.layout = html.Div([html.Div(id="data-loading", style={'display': 'none'}),
                                          style={"margin-top": 0, "padding": 0})],
                                 style={'text-align': 'center'}),
                        html.Hr(),
-
                        html.Div([html.Div([html.Label('Year'),
                                            dropdown_years,
                                            html.Label('Type of Visa(*)',style={"margin-top": "30px"}),
@@ -59,7 +53,7 @@ app.layout = html.Div([html.Div(id="data-loading", style={'display': 'none'}),
                                           className="seven columns")],
                                 className="row"),
                        html.Hr(),
-                       html.Div(
+                       html.Div(  # There is a bug with markdown that does not allow me to indent it PEP-8 properly
                        dcc.Markdown('''
 (*) Work visas include mainly High-value (Tier 1), Skilled (Tier 2), Youth mobility and
 temporary workers (Tier 5), as well as non-PBS/Other work visas.
@@ -83,8 +77,8 @@ def disable_types(selected_outcome):
     :return:
     """
     if selected_outcome == "rejected":
-        dictionaries_of_visas = [{'label': 'Work', 'value': 'work', 'disabled': True}, {'label': 'Study', 'value': 'study',
-                                                                                        'disabled': True},
+        dictionaries_of_visas = [{'label': 'Work', 'value': 'work', 'disabled': True},
+                                 {'label': 'Study', 'value': 'study', 'disabled': True},
                                  {'label': 'All (incl. visitors)', 'value': 'all'}]
     else:
         dictionaries_of_visas = [{'label': 'Work', 'value': 'work'}, {'label': 'Study', 'value': 'study'},
@@ -207,6 +201,12 @@ def update_map(hidden_div):
 @app.callback(dash.dependencies.Output('bar-plot', 'figure'),
               [dash.dependencies.Input('data-loading', 'children')])
 def plot_sorted_countries(hidden_div):
+    """
+    Callback to update a list of top 8 countries with respect to data-loading stats
+
+    :param hidden_div: hidden div with data information
+    :return: plotly barplot
+    """
     hidden_div = json.loads(hidden_div)
 
     data_frame = pd.read_json(hidden_div['df'], orient='split')
@@ -229,20 +229,27 @@ def plot_sorted_countries(hidden_div):
 
 @app.callback(dash.dependencies.Output('graph-total', 'figure'),
               [dash.dependencies.Input('type_of_visa_to_display', 'value')])
-def plot_total_graph(type):
-    data_frame = pd.read_csv('data/total_year_{}.csv'.format(type), index_col=0)
+def plot_total_graph(selected_type):
+    """
+    Callback to update the graph
+
+    :param selected_type:
+    :return: plotly scatter
+    """
+    data_frame = pd.read_csv('data/total_year_{}.csv'.format(selected_type), index_col=0)
 
     x = data_frame.index
     y = data_frame['Total'].get_values()
 
     plotly_data = [go.Scatter(x=x, y=y, line={'color': 'rgb(16, 39, 190)'})]
-    plotly_layout = go.Layout(dict(title="Total number of {} visas per year".format(type)), xaxis={"tickangle": -60,
+    plotly_layout = go.Layout(dict(title="Total number of {} visas per year".format(selected_type)), xaxis={"tickangle": -60,
                                                                                    "tickmode": "array",
                                                                                    "tickvals": np.arange(2005, 2017)})
 
     return dict(data=plotly_data, layout=plotly_layout)
 
 
+# Adds skeleton for a nicer template
 app.css.append_css({
     "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
 })
