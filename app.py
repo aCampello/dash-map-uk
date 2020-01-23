fa55ebc6b71f12d305339284082fbb2b37552b8e
aCampello <a.campello@wellcome.ac.uk>
2020-01-23 09:12:01 +0000

    Add new data (2019) to the map

--- app.py
+++ app.py
@@ -5,18 +5,24 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 import json
+import logging
+import pycountry
+
+logging.basicConfig(level=logging.INFO)
 
 app = dash.Dash()
 app.title = 'Map of Immigration'
 server = app.server
 
 # Main dropdown buttons
-dictionaries_of_years = [{'label': year, 'value': year} for year in np.arange(2005, 2018)]
-dictionaries_of_outcomes = [{'label': 'Granted', 'value': 'granted'}, {'label': 'Rejected', 'value': 'rejected'}]
+dictionaries_of_years = [{'label': year, 'value': year} for year in np.arange(2005, 2019)]
+dictionaries_of_outcomes = [{'label': 'Granted', 'value': 'Issued'},
+                            {'label': 'Rejected', 'value': 'Refused'},
+                            {'label': 'All', 'value': 'All'}]
 
 dropdown_years = dcc.Dropdown(id='year_to_display', options=dictionaries_of_years, value='2016')
 radio_items_type_of_outcome = dcc.RadioItems(id='outcome_of_application', options=dictionaries_of_outcomes,
-                                             value='granted', labelStyle={'display': 'inline-block',
+                                             value='Issued', labelStyle={'display': 'inline-block',
                                                                           'margin-right': 10,
                                                                           'margin-top': 30})
 
@@ -37,10 +43,10 @@ app.layout = html.Div([html.Div(id="data-loading", style={'display': 'none'}),
                                            dropdown_years,
                                            html.Label('Type of Visa(*)',style={"margin-top": "30px"}),
                                            html.Div([dcc.Dropdown(id='type_of_visa_to_display',
-                                                                  options=[{'label': 'Work', 'value': 'work'},
-                                                                           {'label': 'Study', 'value': 'study'},
-                                                                           {'label': 'All (incl. visitors)', 'value': 'all'}],
-                                                                  value='work')],
+                                                                  options=[{'label': 'Work', 'value': 'Work'},
+                                                                           {'label': 'Study', 'value': 'Study'},
+                                                                           {'label': 'All (incl. visitors)', 'value': 'All'}],
+                                                                  value='Work')],
                                                     id='type_of_visa_to_display_div'),
                                            html.P(''), radio_items_type_of_outcome],
                                           className="three columns", style={"margin-top": 40}),
@@ -68,37 +74,45 @@ been made for the missing information.
 ''')
                        )],
                       className='container')
+app.server.logger.info("Reading data from csv file.")
+df = pd.read_csv('data/final_visa_data.csv')
 
 
-@app.callback(dash.dependencies.Output('type_of_visa_to_display', 'options'),
-              [dash.dependencies.Input('outcome_of_application', 'value')])
-def disable_types(selected_outcome):
-    """
-    Callback to disable "work of study" in case of "rejected outcomes
-    :param selected_outcome:
-    :return:
-    """
-    if selected_outcome == "rejected":
-        dictionaries_of_visas = [{'label': 'Work', 'value': 'work', 'disabled': True},
-                                 {'label': 'Study', 'value': 'study', 'disabled': True},
-                                 {'label': 'All (incl. visitors)', 'value': 'all'}]
-    else:
-        dictionaries_of_visas = [{'label': 'Work', 'value': 'work'}, {'label': 'Study', 'value': 'study'},
-                                 {'label': 'All (incl. visitors)', 'value': 'all'}]
+uk_code = pycountry.countries.get(name='United Kingdom').alpha_3
 
-    return dictionaries_of_visas
+all_country_codes = \
+    [{'country_code': country.alpha_3, 'Nationality': '', 'Decisions': 0, 'Region': ''}
+     for country in pycountry.countries.objects]
 
-
-@app.callback(dash.dependencies.Output('type_of_visa_to_display', 'value'),
-              [dash.dependencies.Input('outcome_of_application', 'value')])
-def disable_types(selected_outcome):
-    """
-    Callback to pass to "all" in case of "rejected outcomes"
-    :param selected_outcome:
-    :return:
-    """
-
-    return "all"
+# @app.callback(dash.dependencies.Output('type_of_visa_to_display', 'options'),
+#               [dash.dependencies.Input('outcome_of_application', 'value')])
+# def disable_types(selected_outcome):
+#     """
+#     Callback to disable "work of study" in case of "rejected outcomes
+#     :param selected_outcome:
+#     :return:
+#     """
+#     if selected_outcome == "rejected":
+#         dictionaries_of_visas = [{'label': 'Work', 'value': 'work', 'disabled': True},
+#                                  {'label': 'Study', 'value': 'study', 'disabled': True},
+#                                  {'label': 'All (incl. visitors)', 'value': 'all'}]
+#     else:
+#         dictionaries_of_visas = [{'label': 'Work', 'value': 'work'}, {'label': 'Study', 'value': 'study'},
+#                                  {'label': 'All (incl. visitors)', 'value': 'all'}]
+#
+#     return dictionaries_of_visas
+#
+#
+# @app.callback(dash.dependencies.Output('type_of_visa_to_display', 'value'),
+#               [dash.dependencies.Input('outcome_of_application', 'value')])
+# def disable_types(selected_outcome):
+#     """
+#     Callback to pass to "all" in case of "rejected outcomes"
+#     :param selected_outcome:
+#     :return:
+#     """
+#
+#     return "all"
 
 
 @app.callback(dash.dependencies.Output('data-loading', 'children'),
@@ -123,16 +137,32 @@ def load_data(selected_year, selected_type, selected_outcome):
                "selected_type": selected_type,
                "selected_outcome": selected_outcome}
 
-    # Reads the correct dataframe
-    if selected_outcome == 'rejected':
-        file_to_read = 'map_of_immigration_rejected_' + str(selected_year) + '.csv'
-    else:
-        file_to_read = 'map_of_immigration_' + str(selected_type) + '_' + str(selected_year) + '.csv'
+    filter = df['Year'] == int(selected_year)
+    if selected_type != 'All':
+        filter = filter & (df['Visa type group'] == selected_type)
+    if selected_outcome != 'All':
+        filter = filter & (df['Case outcome'] == selected_outcome)
 
-    data_frame = pd.read_csv('data/' + file_to_read, index_col=0)
+    grouped = df[filter].append(pd.DataFrame(all_country_codes),sort=False).\
+        groupby('country_code').agg({
+         'Decisions': sum,
+         'Region': 'first',
+         'Nationality': 'first'
+        }
+    )
 
+    grouped.reset_index(inplace=True)
+    grouped.rename({
+        'Nationality': 'Country',
+        'Decisions': 'Total',
+        'Region': 'Geographical region'
+    }, axis=1, inplace=True)
     # Dumps a json to be able to share across divs
-    return json.dumps({"df": data_frame.to_json(orient='split'), "filters": filters})
+
+    #import pdb
+    #pdb.set_trace()
+
+    return json.dumps({"df": grouped.to_json(orient='split'), "filters": filters})
 
 
 @app.callback(dash.dependencies.Output('map', 'figure'),
@@ -152,8 +182,8 @@ def update_map(hidden_div):
     filters = hidden_div['filters']
 
     selected_year = filters.get("selected_year", 2016)
-    selected_type = filters.get("selected_type", "all")
-    selected_outcome = filters.get("selected_outcome", "granted")
+    selected_type = filters.get("selected_type", "All")
+    selected_outcome = filters.get("selected_outcome", "Issued")
 
     total = data_frame['Total'].sum()
 
@@ -183,7 +213,7 @@ def update_map(hidden_div):
         colorscale=[[0, "rgb(255, 10, 17)"], [1, "rgb(255, 10, 17)"]],
         showscale=False)])
 
-    selected_type_to_display = (selected_type + ' ' if selected_type != 'all' else '')
+    selected_type_to_display = (selected_type + ' ' if selected_type != 'All' else '')
     plotly_layout = go.Layout(dict(title='Total number of {} {}visas in {}: {:,}.'.format(selected_outcome,
                                                                                           selected_type_to_display,
                                                                                           selected_year,
@@ -208,7 +238,6 @@ def plot_sorted_countries(hidden_div):
     hidden_div = json.loads(hidden_div)
 
     data_frame = pd.read_json(hidden_div['df'], orient='split')
-    filters = hidden_div['filters']
 
     sorted_countries = data_frame.sort_values(by='Total', ascending=False)[:8][['Total', 'Country']].get_values()[::-1]
 
@@ -235,18 +264,26 @@ def plot_total_graph(selected_type, selected_outcome):
     :param selected_type:
     :return: plotly scatter
     """
-    if selected_outcome == 'rejected':
-        selected_type = selected_outcome
 
-    data_frame = pd.read_csv('data/total_year_{}.csv'.format(selected_type), index_col=0)
+    filter = [True]*len(df)
+    if selected_type != 'All':
+        filter = filter & (df['Visa type group'] == selected_type)
+    if selected_outcome != 'All':
+        filter = filter & (df['Case outcome'] == selected_outcome)
 
-    x = data_frame.index
-    y = data_frame['Total'].get_values()
+    grouped = df[filter].groupby('Year').agg({
+        'Decisions': sum,
+    })
+
+    print(grouped)
+
+    x = grouped.index
+    y = grouped['Decisions'].get_values()
 
     plotly_data = [go.Scatter(x=x, y=y, line={'color': 'rgb(16, 39, 190)'})]
     plotly_layout = go.Layout(dict(title="Total number of {} visas per year".format(selected_type)), xaxis={"tickangle": -60,
                                                                                    "tickmode": "array",
-                                                                                   "tickvals": np.arange(2005, 2017)})
+                                                                                   "tickvals": np.arange(2005, 2019)})
 
     return dict(data=plotly_data, layout=plotly_layout)
 
